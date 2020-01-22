793a813157c701f93f00ce9bbd618e05e8bd6acb
Alexander Huber <alih83@gmx.de>
2020-01-22 19:38:05 +0100

    fix mobile layout padding

--- imports/startup/ui/components/Layout.js
+++ imports/startup/ui/components/Layout.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
 import PropTypes from 'prop-types';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { withStyles } from '@material-ui/core/styles';
@@ -11,15 +12,21 @@ const styles = theme => ({
   content: {
     padding: theme.spacing.unit * 5,
   },
+  mobileContent: {
+    paddingTop: theme.spacing.unit * 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
 });
 
 const Layout = ({ children, classes }) => {
+  const matches = useMediaQuery('(min-width:600px)');
   return (
     <div>
       <Navbar />
       <div className={classes.root}>
         <CssBaseline />
-        <div className={classes.content}>{children}</div>
+        {matches ? <div className={classes.content}>{children}</div> : <div className={classes.mobileContent}>{children}</div>}
       </div>
     </div>
   );
